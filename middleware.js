4068486b0823a24fbe753310a5c7063fe7e5b514
Abhishek Kumar Gupta <ag6557168@gmail.com>
2026-05-28 19:25:53 +0530

    review authentication

--- middleware.js
+++ middleware.js
@@ -1,5 +1,6 @@
 const user = require("./models/user");
 const Listing = require("./models/listing")
+const Review = require("./models/review")
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema, reviewSchema} = require("./schema.js");
 
@@ -50,4 +51,14 @@ module.exports.validateReview = (req, res, next) => {
     }else {
         next();
     }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+     let {id,reviewId} = req.params;
+     let listing = await Review.findById(id);
+        if(!review.author.equals(res.locals.currUser._id)) {
+            req.flash("error", "You are not the author of this listing");
+            return res.redirect(`/listings/${id}`);
+        }
+        next();
 }
