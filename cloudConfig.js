6e198d982cf8c27a782797b954739870889b56ea
Abhishek Kumar Gupta <ag6557168@gmail.com>
2026-05-29 20:25:48 +0530

    Project updated

---
+++ cloudConfig.js
@@ -0,0 +1,23 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+console.log("API KEY =", process.env.CLOUD_API_KEY);
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET
+})
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'wanderlust_DEV',
+    allowFormates: ["png", "jpg", "jpeg"],
+  },
+});
+
+
+module.exports ={
+    cloudinary,
+    storage
+}
--- models/listing.js
+++ models/listing.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const Review = require("./review.js");
+const { string } = require("joi");
 
 const listingSchema = new Schema({
     title: {
@@ -9,12 +10,8 @@ const listingSchema = new Schema({
     },
     description: String,
      image: {
-        type: String,
-        default: "https://in.images.search.yahoo.com/search/images?p=images+unsplash&fr=mcafee&type=E210IN1589G0&imgurl=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1506038634487-60a69ae4b7b1%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DMnwxMjA3fDB8MXxzZWFyY2h8NHx8ZnJlZXx8MHx8fHwxNjIzOTU3NjIy%26ixlib%3Drb-1.2.1%26q%3D80%26w%3D1080#id=1&iurl=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1506038634487-60a69ae4b7b1%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DMnwxMjA3fDB8MXxzZWFyY2h8NHx8ZnJlZXx8MHx8fHwxNjIzOTU3NjIy%26ixlib%3Drb-1.2.1%26q%3D80%26w%3D1080&action=click",
-        set: (v) =>
-             v === ""
-                ? "https://in.images.search.yahoo.com/search/images?p=images+unsplash&fr=mcafee&type=E210IN1589G0&imgurl=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1506038634487-60a69ae4b7b1%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DMnwxMjA3fDB8MXxzZWFyY2h8NHx8ZnJlZXx8MHx8fHwxNjIzOTU3NjIy%26ixlib%3Drb-1.2.1%26q%3D80%26w%3D1080#id=1&iurl=http%3A%2F%2Fimages.unsplash.com%2Fphoto-1506038634487-60a69ae4b7b1%3Fcrop%3Dentropy%26cs%3Dtinysrgb%26fit%3Dmax%26fm%3Djpg%26ixid%3DMnwxMjA3fDB8MXxzZWFyY2h8NHx8ZnJlZXx8MHx8fHwxNjIzOTU3NjIy%26ixlib%3Drb-1.2.1%26q%3D80%26w%3D1080&action=click"
-                : v
+        url: String,
+        filename: String,
     },
     price: {
             type: Number,
