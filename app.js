f5e19b72acec2b0cd05f5125890a70e161bdda0b
Abhishek Kumar Gupta <ag6557168@gmail.com>
2026-06-01 18:08:10 +0530

    Apply Changeses

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ const listingRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js")
 const userRouter = require("./routes/user.js")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+//const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+const dbUrl  =  process.env.ATLASDB_URL;
 
 main()
 .then(() => {
@@ -27,7 +29,7 @@ main()
 })
 
 async function main() {
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(dbUrl)
 }
 
 app.set("view engine", "ejs");
